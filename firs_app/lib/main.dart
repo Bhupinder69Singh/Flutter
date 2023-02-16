aab9c4fc36aad3c312ac210b5b203905bd6b6d70
Bhupinder Singh <bhupindersinghsahmey@gmail>
2023-02-16 23:27:14 +0530

    initial changes

---
+++ firs_app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // in flutter we have mainly 3types of app i.e MaterialApp,WidgetApp,Cupertino app
+    //so 1st we need to specify
+    return Container();
+  }
+}
